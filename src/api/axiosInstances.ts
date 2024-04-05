434881ab9bc4900ccbeb11aa562dcefb3aca3f16
SySagar <sysagar07@gmail.com>
2024-04-05 22:06:38 +0530

    chore: env

--- src/api/axiosInstances.ts
+++ src/api/axiosInstances.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: process.env.URL,
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${localStorage.getItem("token")}`,
